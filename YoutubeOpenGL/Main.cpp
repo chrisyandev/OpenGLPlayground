544bb4fcab698ff64c71f0968b557455b82952c3
chrisyandev <czyanhome@hotmail.com>
2023-12-19 18:26:09 -0800

    Tutorial 3 - Index Buffer

--- YoutubeOpenGL/Main.cpp
+++ YoutubeOpenGL/Main.cpp
@@ -23,13 +23,6 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // OpenGL 3.3
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // using the CORE profile so that we only have modern functions
 
-	GLfloat vertices[] = // coordinates of vertices
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // lower left
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // lower right
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // upper middle
-	};
-
 	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL); // create a window of 800 by 800 pixels with a title
 	if (window == NULL)
 	{
@@ -59,33 +52,51 @@ int main()
 	glDeleteShader(vertexShader); // no longer needed so delete these
 	glDeleteShader(fragmentShader);
 
-	GLuint VAO, VBO; // containers for Vertex Array and Vertex Buffer Objects
+	GLfloat vertices[] = // coordinates of vertices
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // outer bottom left
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // outer bottom right
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // outer top middle
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // inner top left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // inner top right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // inner bottom middle
+	};
+
+	GLuint indices[] =
+	{
+		0, 3, 5, // lower left triangle
+		3, 2, 4, // lower right triangle
+		5, 4, 1 // upper middle triangle
+	};
+
+	GLuint VAO, VBO, EBO; // containers for Vertex Array Object, Vertex Buffer Object, Element Buffer Object
 
 	glGenVertexArrays(1, &VAO); // generate array with 1 element
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO); // set the current Vertex Array Object
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); // set the current Vertex Buffer Object
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // store vertices inside buffer
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // set the current Element Buffer Object
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // store indices inside buffer
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // define how OpenGL should read the VBO
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0); // makes sure we don't accidentally modify VBO or VAO
-	glBindVertexArray(0);
-
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // background color
-	glClear(GL_COLOR_BUFFER_BIT); // clean the back buffer and assign new color to it
-	glfwSwapBuffers(window); // swap back buffer with front buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO so we don't accidentally modify
+	glBindVertexArray(0); // unbind VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // unbind EBO // unbinding before VAO prevents VAO from using this EBO
 
 	while (!glfwWindowShouldClose(window)) // main loop
 	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // background color
+		glClear(GL_COLOR_BUFFER_BIT); // clean the back buffer and assign new color to it
 		glUseProgram(shaderProgram); // set the Shader Program we want to use
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3); // draw the triangle using GL_TRIANGLES primitive
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0); // draw the triangle using GL_TRIANGLES primitive
 		glfwSwapBuffers(window); // makes sure image is updated each frame
 
 		glfwPollEvents(); // takes care of all GLFW events
@@ -93,6 +104,7 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO); // delete all objects we created
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window); // delete window
