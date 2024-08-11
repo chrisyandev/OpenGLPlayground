0e2b9a7c62d3bf46710c31f83e16610047c3928d
Chris Yan <50633954+chrisyandev@users.noreply.github.com>
2024-08-10 21:50:31 -0700

    Interpolate different vertex colors

--- OpenGLPlayground/Resources/fragShader.glsl
+++ OpenGLPlayground/Resources/fragShader.glsl
@@ -5,7 +5,9 @@ out vec4 color;
 uniform mat4 mv_matrix;
 uniform mat4 p_matrix;
 
+in vec4 varyingColor;
+
 void main(void)
 {
-    color = vec4(1.0, 0.0, 0.0, 1.0);
+    color = varyingColor;
 }
--- OpenGLPlayground/Resources/vertShader.glsl
+++ OpenGLPlayground/Resources/vertShader.glsl
@@ -5,7 +5,10 @@ layout (location=0) in vec3 position;
 uniform mat4 mv_matrix;
 uniform mat4 p_matrix;
 
+out vec4 varyingColor;
+
 void main(void)
 {
     gl_Position = p_matrix * mv_matrix * vec4(position, 1.0);
+    varyingColor = vec4(position, 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5); // multiplies the location by 1/2 and then adds 1/2 to convert the range of values from [-1..+1] to [0..1]
 }
