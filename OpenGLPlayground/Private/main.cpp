ef58aecd32106a28a3cd5ad21312efd09f477640
Chris Yan <50633954+chrisyandev@users.noreply.github.com>
2024-08-12 00:43:32 -0700

    Instancing 24 animated cubes

--- OpenGLPlayground/Private/main.cpp
+++ OpenGLPlayground/Private/main.cpp
@@ -16,16 +16,15 @@ constexpr GLuint NUM_VAOS = 1;
 constexpr GLuint NUM_VBOS = 2;
 
 float cameraX, cameraY, cameraZ;
-float cubePosX, cubePosY, cubePosZ;
 GLuint renderingProgram;
 GLuint vao[NUM_VAOS];
 GLuint vbo[NUM_VBOS];
 
 // allocate variables used in display() function, so that they won’t need to be allocated during rendering
-GLuint mvLoc, pLoc;
-int width, height, cubeCounter;
-float aspect, timeFactor;
-glm::mat4 pMat, vMat, mMat, mvMat, tMat, rMat;
+GLuint pLoc, vLoc, cTimeLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat;
 
 void setupVertices() // 36 vertices, 12 triangles, makes 2x2x2 cube placed at origin
 {
@@ -61,7 +60,6 @@ void init(GLFWwindow* window)
     renderingProgram = Utils::createShaderProgram(vertShaderPath.c_str(), fragShaderPath.c_str());
 
     cameraX = 0.0f; cameraY = 0.0f; cameraZ = 32.0f;
-    cubePosX = 0.0f; cubePosY = -2.0f; cubePosZ = 0.0f; // shift down Y to reveal perspective
 
     setupVertices();
 }
@@ -72,46 +70,31 @@ void display(GLFWwindow* window, double currentTime)
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(renderingProgram);
 
-    // get the uniform variables for the MV and projection matrices
-    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
-    pLoc = glGetUniformLocation(renderingProgram, "p_matrix");
-
     // build perspective matrix
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float)width / (float)height;
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
 
-    // view matrix
+    // build view matrix
     vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
 
-    for (cubeCounter = 0; cubeCounter < 24; ++cubeCounter)
-    {
-        timeFactor = currentTime + cubeCounter;
+    // copy data to vertex shader so it can build M and MV matrices
+    pLoc = glGetUniformLocation(renderingProgram, "p_matrix");
+    glUniformMatrix4fv(pLoc, 1, GL_FALSE, glm::value_ptr(pMat)); // copy perspective matrix
+    vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+    glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat)); // copy view matrix
+    cTimeLoc = glGetUniformLocation(renderingProgram, "cTime");
+    glUniform1f(cTimeLoc, (float)currentTime); // copy current time
 
-        // use current time to compute different translations in x, y, and z
-        tMat = glm::translate(glm::mat4(1.0f), glm::vec3(sin(0.35f * timeFactor) * 8.0f, cos(0.52f * timeFactor) * 8.0f, sin(0.7f * timeFactor) * 8.0f)); // y(t)=A*sin(stretch*t)
-        rMat = glm::rotate(glm::mat4(1.0f), 1.75f * timeFactor, glm::vec3(0.0f, 1.0f, 0.0f));
-        rMat = glm::rotate(rMat, 1.75f * timeFactor, glm::vec3(1.0f, 0.0f, 0.0f)); // the 1.75 adjusts the rotation speed
-        rMat = glm::rotate(rMat, 1.75f * timeFactor, glm::vec3(0.0f, 0.0f, 1.0f));
+    // associate VBO with the corresponding vertex attribute in the vertex shader
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
 
-        // model and model-view matrix
-        mMat = tMat * rMat;
-        mvMat = vMat * mMat;
-
-        // copy perspective and MV matrices to corresponding uniform variables
-        glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-        glUniformMatrix4fv(pLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-
-        // associate VBO with the corresponding vertex attribute in the vertex shader
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-        glEnableVertexAttribArray(0);
-
-        // adjust OpenGL settings and draw model
-        glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LEQUAL);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
+    // adjust OpenGL settings and draw model
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 24);
 }
 
 int main(void)
--- OpenGLPlayground/Resources/vertShader.glsl
+++ OpenGLPlayground/Resources/vertShader.glsl
@@ -2,13 +2,74 @@
 
 layout (location=0) in vec3 position;
 
-uniform mat4 mv_matrix;
+uniform mat4 v_matrix;
 uniform mat4 p_matrix;
+uniform float cTime; // current time for animation and placement of cubes
 
 out vec4 varyingColor;
 
+mat4 buildTranslate(float x, float y, float z); // declaration of matrix transformation utility functions
+mat4 buildRotateX(float rad);
+mat4 buildRotateY(float rad);
+mat4 buildRotateZ(float rad);
+
 void main(void)
 {
+    float timeFactor = gl_InstanceID + cTime; // value based on current time, but different for each cube instance
+    float a = sin(0.35 * timeFactor) * 8.0; // these are the x, y, and z components for the translation below
+    float b = sin(0.52 * timeFactor) * 8.0;
+    float c = sin(0.70 * timeFactor) * 8.0;
+
+    // build the rotation and translation matrices to be applied to this cube’s model matrix
+    mat4 localRotX = buildRotateX(1.75 * timeFactor);
+    mat4 localRotY = buildRotateY(1.75 * timeFactor);
+    mat4 localRotZ = buildRotateZ(1.75 * timeFactor);
+    mat4 localTrans = buildTranslate(a, b, c);
+
+    // build the model matrix and then the model-view matrix
+    mat4 m_matrix = localTrans * localRotX * localRotY * localRotZ;
+    mat4 mv_matrix = v_matrix * m_matrix;
+
     gl_Position = p_matrix * mv_matrix * vec4(position, 1.0);
     varyingColor = vec4(position, 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5); // multiplies the location by 1/2 and then adds 1/2 to convert the range of values from [-1..+1] to [0..1]
+}
+
+// builds and returns a translation matrix
+mat4 buildTranslate(float x, float y, float z)
+{
+    mat4 trans = mat4(1.0, 0.0, 0.0, 0.0,
+                      0.0, 1.0, 0.0, 0.0,
+                      0.0, 0.0, 1.0, 0.0,
+                      x, y, z, 1.0);
+    return trans;
+}
+
+// builds and returns a matrix that performs a rotation around the X axis
+mat4 buildRotateX(float rad)
+{
+    mat4 xrot = mat4(1.0, 0.0, 0.0, 0.0,
+                     0.0, cos(rad), -sin(rad), 0.0,
+                     0.0, sin(rad), cos(rad), 0.0,
+                     0.0, 0.0, 0.0, 1.0);
+    return xrot;
+}
+
+// builds and returns a matrix that performs a rotation around the Y axis
+mat4 buildRotateY(float rad)
+{
+    mat4 yrot = mat4(cos(rad), 0.0, sin(rad), 0.0,
+                     0.0, 1.0, 0.0, 0.0,
+                     -sin(rad), 0.0, cos(rad), 0.0,
+                     0.0, 0.0, 0.0, 1.0);
+    return yrot;
+}
+
+// builds and returns a matrix that performs a rotation around the Z axis
+mat4 buildRotateZ(float rad)
+{
+    mat4 zrot = mat4(cos(rad), -sin(rad), 0.0, 0.0,
+                     sin(rad), cos(rad), 0.0, 0.0,
+                     0.0, 0.0, 1.0, 0.0,
+                     0.0, 0.0, 0.0, 1.0);
+    return zrot;
 }
